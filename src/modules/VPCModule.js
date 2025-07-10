fd47b5b1d5643975c77e9b57190792801aad3e01
SAIKRUSHNAGOUDA <74015150+SAIKRUSHNAGOUDA@users.noreply.github.com>
2025-07-10 14:06:46 +0530

    Update VPCModule.js

--- src/modules/VPCModule.js
+++ src/modules/VPCModule.js
@@ -1,9 +1,7 @@
-const VPCModule = (id) => {
+export function generateVPC(id, config) {
   return `resource "aws_vpc" "${id}" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block           = "${config.cidr_block || "10.0.0.0/16"}"
   enable_dns_support   = true
   enable_dns_hostnames = true
-}`;
-};
-
-export default VPCModule;
+}\n\n`;
+}
