a07379d06dacebc59d24d28156c3d9258e6878fa
SAIKRUSHNAGOUDA <74015150+SAIKRUSHNAGOUDA@users.noreply.github.com>
2025-07-10 14:15:31 +0530

    Update Sidebar.js

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,3 +1,4 @@
+// Sidebar.js
 import React from "react";
 
 const Sidebar = () => {
@@ -7,33 +8,37 @@ const Sidebar = () => {
   };
 
   return (
-    <aside className="w-48 bg-gray-200 p-4 space-y-2">
+    <aside className="w-48 bg-gray-200 p-4">
+      <h2 className="text-lg font-bold mb-4">Components</h2>
+
       <div
-        className="p-2 bg-white rounded shadow cursor-pointer flex items-center space-x-2"
+        className="p-2 mb-2 bg-white rounded shadow cursor-pointer flex items-center space-x-2"
         onDragStart={(e) => handleDragStart(e, "ec2")}
         draggable
       >
-        {/* <img src="/icons/ec2.png" alt="EC2" className="w-5 h-5" /> */}
+        <img src="/icons/ec2.png" alt="EC2" className="w-6 h-6" />
         <span>EC2</span>
       </div>
 
       <div
-        className="p-2 bg-white rounded shadow cursor-pointer flex items-center space-x-2"
+        className="p-2 mb-2 bg-white rounded shadow cursor-pointer flex items-center space-x-2"
         onDragStart={(e) => handleDragStart(e, "s3")}
         draggable
       >
-        {/* <img src="/icons/s3.png" alt="S3" className="w-5 h-5" /> */}
+        <img src="/icons/s3.png" alt="S3" className="w-6 h-6" />
         <span>S3</span>
       </div>
 
       <div
-        className="p-2 bg-white rounded shadow cursor-pointer flex items-center space-x-2"
+        className="p-2 mb-2 bg-white rounded shadow cursor-pointer flex items-center space-x-2"
         onDragStart={(e) => handleDragStart(e, "vpc")}
         draggable
       >
-        {/* <img src="/icons/vpc.png" alt="VPC" className="w-5 h-5" /> */}
+        <img src="/icons/vpc.png" alt="VPC" className="w-6 h-6" />
         <span>VPC</span>
       </div>
+
+      {/* Add more module components here */}
     </aside>
   );
 };
