9e7ca225e06949996ff65992e42522050adb7b10
SAIKRUSHNAGOUDA <74015150+SAIKRUSHNAGOUDA@users.noreply.github.com>
2025-07-10 14:07:10 +0530

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,20 +1,19 @@
-import EC2Module from "./modules/EC2Module";
-import S3Module from "./modules/S3Module";
-import VPCModule from "./modules/VPCModule";
+import { generateEC2 } from "./modules/EC2Module";
+import { generateS3 } from "./modules/S3Module";
+import { generateVPC } from "./modules/VPCModule";
 
 export function generateTerraformCode(elements, configMap = {}) {
   let code = "";
 
   elements.forEach((el) => {
-    const id = el.id;
-    const config = configMap[id] || {};
+    const config = configMap[el.id] || {};
 
-    if (id.startsWith("ec2")) {
-      code += EC2Module(id, config) + "\n\n";
-    } else if (id.startsWith("s3")) {
-      code += S3Module(id, config) + "\n\n";
-    } else if (id.startsWith("vpc")) {
-      code += VPCModule(id, config) + "\n\n";
+    if (el.id.startsWith("ec2")) {
+      code += generateEC2(el.id, config);
+    } else if (el.id.startsWith("s3")) {
+      code += generateS3(el.id, config);
+    } else if (el.id.startsWith("vpc")) {
+      code += generateVPC(el.id, config);
     }
   });
 
