ae48cbc299c78c0aff856d4794ca694cac801cd8
SAIKRUSHNAGOUDA <74015150+SAIKRUSHNAGOUDA@users.noreply.github.com>
2025-07-10 14:15:10 +0530

    Update S3Module.js

--- src/modules/S3Module.js
+++ src/modules/S3Module.js
@@ -1,6 +1,6 @@
 export function generateS3(id, config) {
   return `resource "aws_s3_bucket" "${id}" {
-  bucket = "${id}-bucket"
+  bucket = "${config.bucket || `${id}-bucket`}"
   acl    = "${config.acl || "private"}"
 }\n\n`;
 }
