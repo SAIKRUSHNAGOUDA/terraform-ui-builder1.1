6278df73b5888a29d10f22cba8b0936055fddf89
SAIKRUSHNAGOUDA <74015150+SAIKRUSHNAGOUDA@users.noreply.github.com>
2025-07-10 12:56:40 +0530

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,13 @@
-export function generateTerraformCode(elements) {
+export function generateTerraformCode(elements, configMap = {}) {
   let code = "";
 
   elements.forEach((el) => {
+    const config = configMap[el.id] || {};
+
     if (el.id.startsWith("ec2")) {
       code += `resource "aws_instance" "${el.id}" {
-  ami           = "ami-12345678"
-  instance_type = "t2.micro"
+  ami           = "${config.ami || "ami-12345678"}"
+  instance_type = "${config.instance_type || "t2.micro"}"
 }\n\n`;
     } else if (el.id.startsWith("s3")) {
       code += `resource "aws_s3_bucket" "${el.id}" {
@@ -17,9 +19,6 @@ export function generateTerraformCode(elements) {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
-  tags = {
-    Name = "${el.id}-vpc"
-  }
 }\n\n`;
     }
   });
