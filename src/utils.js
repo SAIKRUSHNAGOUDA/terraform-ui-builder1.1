ea0c461720cfa4dafc1f6dfd326900c500bc72f9
SAIKRUSHNAGOUDA <74015150+SAIKRUSHNAGOUDA@users.noreply.github.com>
2025-07-10 13:20:37 +0530

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,25 +1,20 @@
+import EC2Module from "./modules/EC2Module";
+import S3Module from "./modules/S3Module";
+import VPCModule from "./modules/VPCModule";
+
 export function generateTerraformCode(elements, configMap = {}) {
   let code = "";
 
   elements.forEach((el) => {
-    const config = configMap[el.id] || {};
+    const id = el.id;
+    const config = configMap[id] || {};
 
-    if (el.id.startsWith("ec2")) {
-      code += `resource "aws_instance" "${el.id}" {
-  ami           = "${config.ami || "ami-12345678"}"
-  instance_type = "${config.instance_type || "t2.micro"}"
-}\n\n`;
-    } else if (el.id.startsWith("s3")) {
-      code += `resource "aws_s3_bucket" "${el.id}" {
-  bucket = "${el.id}-bucket"
-  acl    = "private"
-}\n\n`;
-    } else if (el.id.startsWith("vpc")) {
-      code += `resource "aws_vpc" "${el.id}" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-}\n\n`;
+    if (id.startsWith("ec2")) {
+      code += EC2Module(id, config) + "\n\n";
+    } else if (id.startsWith("s3")) {
+      code += S3Module(id, config) + "\n\n";
+    } else if (id.startsWith("vpc")) {
+      code += VPCModule(id, config) + "\n\n";
     }
   });
 
