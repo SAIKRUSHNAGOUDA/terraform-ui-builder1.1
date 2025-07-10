ecffb5333433e1051b4d7a6c9f9fc62dfc6c5a3d
SAIKRUSHNAGOUDA <74015150+SAIKRUSHNAGOUDA@users.noreply.github.com>
2025-07-10 14:06:04 +0530

    Update EC2Module.js

--- src/modules/EC2Module.js
+++ src/modules/EC2Module.js
@@ -1,8 +1,6 @@
-const EC2Module = (id, config = {}) => {
+export function generateEC2(id, config) {
   return `resource "aws_instance" "${id}" {
   ami           = "${config.ami || "ami-12345678"}"
   instance_type = "${config.instance_type || "t2.micro"}"
-}`;
-};
-
-export default EC2Module;
+}\n\n`;
+}
