9948148c022592f2b434a41fe94c8f00c968069d
SAIKRUSHNAGOUDA <74015150+SAIKRUSHNAGOUDA@users.noreply.github.com>
2025-07-11 16:29:06 +0530

    Create regionMap.js

---
+++ src/regionMap.js
@@ -0,0 +1,14 @@
+export const REGION_OPTIONS = {
+  aws: [
+    { label: "US East (N. Virginia)", value: "us-east-1" },
+    { label: "US West (Oregon)", value: "us-west-2" },
+  ],
+  azure: [
+    { label: "East US", value: "East US" },
+    { label: "West Europe", value: "West Europe" },
+  ],
+  gcp: [
+    { label: "us-central1", value: "us-central1" },
+    { label: "europe-west1", value: "europe-west1" },
+  ],
+};
