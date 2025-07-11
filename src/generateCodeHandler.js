daeac0819fe5073a61268db84e08aa3f380e74a8
SAIKRUSHNAGOUDA <74015150+SAIKRUSHNAGOUDA@users.noreply.github.com>
2025-07-11 15:13:19 +0530

    Update generateCodeHandler.js

--- src/generateCodeHandler.js
+++ src/generateCodeHandler.js
@@ -1,7 +1,7 @@
 export const generateTerraformCode = (nodes, configMap, platform, region) => {
   let code = "";
 
-  // Detect platforms
+  // Detect platforms used
   const platformsUsed = new Set(
     nodes.map((node) => configMap[node.id]?.platform || platform)
   );
@@ -15,7 +15,7 @@ export const generateTerraformCode = (nodes, configMap, platform, region) => {
         code += `provider "azurerm" {\n  features = {}\n}\n\n`;
         break;
       case "gcp":
-        code += `provider "google" {\n  region = "${region}"\n}\n\n`;
+        code += `provider "google" {\n  region  = "${region}"\n  project = "your-gcp-project-id"\n}\n\n`; // Replace with actual project ID
         break;
       default:
         code += `# Unsupported platform: ${p}\n\n`;
@@ -60,11 +60,11 @@ resource "azurerm_network_interface" "${node.id}_nic" {
 }
 
 resource "azurerm_linux_virtual_machine" "${node.id}" {
-  name                  = "my-vm"
-  location              = "${config.region}"
-  resource_group_name   = "my-group"
-  vm_size                  = "Standard_B1s"
-  admin_username        = "adminuser"
+  name                = "my-vm"
+  location            = "${config.region}"
+  resource_group_name = "my-group"
+  vm_size             = "Standard_B1s"
+  admin_username      = "adminuser"
 
   network_interface_ids = [
     azurerm_network_interface.${node.id}_nic.id
@@ -82,61 +82,89 @@ resource "azurerm_linux_virtual_machine" "${node.id}" {
     sku       = "18.04-LTS"
     version   = "latest"
   }
-}\n\n`;
+}
+`;
         break;
 
       case "azure-storage":
-        code += `resource "azurerm_storage_account" "${node.id}" {
+        code += `
+resource "azurerm_storage_account" "${node.id}" {
   name                     = "mystorageacct"
   location                 = "${config.region}"
   resource_group_name      = "my-group"
   account_tier             = "Standard"
   account_replication_type = "LRS"
-}\n\n`;
+}
+`;
         break;
 
       case "gcp-compute":
-        code += `resource "google_compute_instance" "${node.id}" {
+        code += `
+resource "google_compute_instance" "${node.id}" {
   name         = "my-instance"
   zone         = "${config.region}-a"
   machine_type = "f1-micro"
-}\n\n`;
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network       = "default"
+    access_config {}
+  }
+}
+`;
         break;
 
       case "gcp-bucket":
-        code += `resource "google_storage_bucket" "${node.id}" {
-  name     = "my-bucket"
-  location = "${config.region}"
-}\n\n`;
+        code += `
+resource "google_storage_bucket" "${node.id}" {
+  name                        = "my-bucket"
+  location                    = "${config.region}"
+  force_destroy               = true
+  uniform_bucket_level_access = true
+}
+`;
         break;
 
       case "gcp-network":
-        code += `resource "google_compute_network" "${node.id}" {
+        code += `
+resource "google_compute_network" "${node.id}" {
   name = "my-network"
-}\n\n`;
+}
+`;
         break;
 
       case "aws-ec2":
-        code += `resource "aws_instance" "${node.id}" {
+        code += `
+resource "aws_instance" "${node.id}" {
   ami           = "ami-123456"
   instance_type = "t2.micro"
   region        = "${config.region}"
-}\n\n`;
+}
+`;
         break;
 
       case "aws-s3":
-        code += `resource "aws_s3_bucket" "${node.id}" {
+        code += `
+resource "aws_s3_bucket" "${node.id}" {
   bucket = "my-bucket"
   acl    = "private"
-}\n\n`;
+}
+`;
         break;
 
       case "aws-vpc":
-        code += `resource "aws_vpc" "${node.id}" {
+        code += `
+resource "aws_vpc" "${node.id}" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
-}\n\n`;
+}
+`;
         break;
 
       default:
